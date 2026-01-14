c4d4c08cdb0735e724179dc89859334360293d92
Roman Korenchuk <ikorew@gmail.com>
2026-01-14 01:25:41 +0100

    Fix i18n in pages for SSR prerendering

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1>{{ $t('pages.home.title') }}</h1>
-    <p>{{ $t('pages.home.description') }}</p>
+    <h1>{{ t('pages.home.title') }}</h1>
+    <p>{{ t('pages.home.description') }}</p>
   </div>
 </template>
+
+<script setup>
+const { t } = useI18n()
+</script>
