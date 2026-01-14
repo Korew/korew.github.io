80aa8404307d5b75ccea289463af4e7a8ce69398
Roman Korenchuk <ikorew@gmail.com>
2026-01-14 01:35:07 +0100

    Fix i18n for SSR - use useI18n() composable

--- app/components/app/Footer.vue
+++ app/components/app/Footer.vue
@@ -1,9 +1,10 @@
 <template>
   <footer>
-    <p>{{ $t('footer.copyright', { year }) }}</p>
+    <p>{{ t('footer.copyright', { year }) }}</p>
   </footer>
 </template>
 
 <script setup>
+const { t } = useI18n()
 const year = new Date().getFullYear()
 </script>
--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1>{{ $t('pages.contact.title') }}</h1>
-    <p>{{ $t('pages.contact.description') }}</p>
+    <h1>{{ t('pages.contact.title') }}</h1>
+    <p>{{ t('pages.contact.description') }}</p>
   </div>
 </template>
+
+<script setup>
+const { t } = useI18n()
+</script>
